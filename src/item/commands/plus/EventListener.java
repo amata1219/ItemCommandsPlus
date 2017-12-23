69eab247da99525b6fa4885c54abda8e46fc9d0b
amata1219 <suzukiamata@gmail.com>
2017-12-23 14:39:34 +0900

    update.

--- src/item/commands/plus/EventListener.java
+++ src/item/commands/plus/EventListener.java
@@ -1,7 +1,34 @@
 package item.commands.plus;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class EventListener implements Listener{
 
+	private ItemCommandsPlus plugin = ItemCommandsPlus.plugin;
+	private CustomConfig config = ItemCommandsPlus.config;
+
+	@EventHandler
+	public void onPlayerUseCommandItemEvent(PlayerUseCommandItemEvent e){
+
+	}
+
+	@EventHandler
+	public void onPlayerInteractEvent(PlayerInteractEvent e){
+		Player p = e.getPlayer();
+	}
+
+	@EventHandler
+	public void onPlayerInteractEntityEvent(PlayerInteractEntityEvent e){
+
+	}
+
+	@EventHandler
+	public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent e){
+
+	}
 }
